1225c55364cc3652819f7865653ad593561eea64
Balázs Németh <6349914+abalage@users.noreply.github.com>
2025-03-15 22:07:52 +0100

    refact: standardize oop code

--- src/whatsthedamage/data_frame_formatter.py
+++ src/whatsthedamage/data_frame_formatter.py
@@ -12,34 +12,41 @@ class DataFrameFormatter:
         """
         Initializes the DataFrameFormatter with default settings.
         Attributes:
-            nowrap (bool): If True, disables text wrapping. Default is False.
-            no_currency_format (bool): If True, disables currency formatting. Default is False.
+            _nowrap (bool): If True, disables text wrapping. Default is False.
+            _no_currency_format (bool): If True, disables currency formatting. Default is False.
         """
-
-        self.nowrap: bool = False
-        self.no_currency_format: bool = False
+        self._nowrap: bool = False
+        self._no_currency_format: bool = False
 
     def set_nowrap(self, nowrap: bool) -> None:
-        self.nowrap = nowrap
+        """
+        Set the nowrap attribute.
+
+        :param nowrap: If True, disables text wrapping.
+        """
+        self._nowrap = nowrap
 
     def set_no_currency_format(self, no_currency_format: bool) -> None:
-        self.no_currency_format = no_currency_format
+        """
+        Set the no_currency_format attribute.
+
+        :param no_currency_format: If True, disables currency formatting.
+        """
+        self._no_currency_format = no_currency_format
 
     def format_dataframe(self, data_for_pandas: Dict[str, Dict[str, float]]) -> pd.DataFrame:
         """
         Formats a given dictionary of data into a pandas DataFrame with optional currency formatting.
-        Args:
-            data_for_pandas (Dict[str, Dict[str, float]]): The input data to be formatted into a DataFrame.
-                The outer dictionary keys represent the columns, and the inner dictionary keys represent the rows.
-        Returns:
-            pd.DataFrame: The formatted DataFrame with optional currency formatting.
-        """
 
+        :param data_for_pandas: The input data to be formatted into a DataFrame.
+            The outer dictionary keys represent the columns, and the inner dictionary keys represent the rows.
+        :return: The formatted DataFrame with optional currency formatting.
+        """
         # Set pandas to display all columns and rows without truncation
         pd.set_option('display.max_columns', None)
         pd.set_option('display.max_rows', None)
         pd.set_option('display.width', 130)
-        if self.nowrap:
+        if self._nowrap:
             pd.set_option('display.expand_frame_repr', False)
 
         # Create a DataFrame from the data
@@ -49,7 +56,7 @@ class DataFrameFormatter:
         df = df.sort_index()
 
         # Format the DataFrame with currency values
-        if not self.no_currency_format:
+        if not self._no_currency_format:
             def format_currency(value: Optional[float]) -> str:
                 if value is None:
                     return 'N/A'

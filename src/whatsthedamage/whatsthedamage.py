5059281ff1284897671a6e61d07f086c507b5110
Balázs Németh <6349914+abalage@users.noreply.github.com>
2025-03-16 21:03:22 +0100

    refact: implement oop best practices

--- src/whatsthedamage/whatsthedamage.py
+++ src/whatsthedamage/whatsthedamage.py
@@ -2,21 +2,16 @@
 This module processes KHBHU CSV files and provides a CLI tool to categorize and summarize the data.
 
 Functions:
-    load_config(config_path: str) -> dict[str, dict[str, dict[str, str]]]:
-        Loads the configuration file and validates its contents.
-
     set_locale(locale_str: str) -> None:
         Sets the locale for currency formatting.
 
-    main(args: dict[str, str | bool | None]) -> str | None:
+    main(args: AppArgs) -> str | None:
         The main function receives arguments, loads the configuration, reads the CSV file,
         processes the rows, and prints or saves the result.
 """
 import locale
 import sys
-from whatsthedamage.csv_file_reader import CsvFileReader
-from whatsthedamage.rows_processor import RowsProcessor
-from whatsthedamage.data_frame_formatter import DataFrameFormatter
+from whatsthedamage.csv_processor import CSVProcessor
 from whatsthedamage.config import AppArgs, load_config
 
 
@@ -24,7 +19,12 @@ __all__ = ['main']
 
 
 def set_locale(locale_str: str) -> None:
-    # Setting locale
+    """
+    Sets the locale for currency formatting.
+
+    Args:
+        locale_str (str): The locale string to set.
+    """
     try:
         locale.setlocale(locale.LC_ALL, locale_str)
     except locale.Error:
@@ -33,50 +33,22 @@ def set_locale(locale_str: str) -> None:
 
 
 def main(args: AppArgs) -> str | None:
+    """
+    The main function receives arguments, loads the configuration, reads the CSV file,
+    processes the rows, and prints or saves the result.
+
+    Args:
+        args (AppArgs): The application arguments.
+
+    Returns:
+        str | None: The formatted result as a string or None.
+    """
     # Load the configuration file
     config = load_config(str(args['config']))
 
     # Set the locale for currency formatting
     set_locale(config.main.locale)
 
-    # Create a CsvReader object and read the file contents
-    csv_reader = CsvFileReader(
-        str(args['filename']),
-        str(config.csv.dialect),
-        str(config.csv.delimiter),
-        dict(config.csv.attribute_mapping)
-    )
-    csv_reader.read()
-    rows = csv_reader.get_rows()
-
-    # Process the rows
-    processor = RowsProcessor()
-
-    # Pass the configuration to the processor
-    processor.set_date_attribute_format(config.csv.date_attribute_format)
-    processor.set_cfg_pattern_sets(config.enricher_pattern_sets)
-
-    # Pass the arguments to the processor
-    processor.set_start_date(args.get('start_date'))
-    processor.set_end_date(args.get('end_date'))
-    processor.set_verbose(args.get('verbose', False))
-    processor.set_category(args.get('category', 'category'))
-    processor.set_filter(args.get('filter'))
-
-    data_for_pandas = processor.process_rows(rows)
-
-    # Create an instance of DataFrameFormatter
-    formatter = DataFrameFormatter()
-    formatter.set_nowrap(args.get('nowrap', False))
-    formatter.set_no_currency_format(args.get('no_currency_format', False))
-
-    # Format the DataFrame
-    df = formatter.format_dataframe(data_for_pandas)
-
-    # Different output format depending on client request
-    if args.get('output_format') == 'html':
-        return df.to_html(border=0)
-    elif args.get('output'):
-        return df.to_csv(args.get('output'), index=True, header=True, sep=';', decimal=',')
-    else:
-        return df.to_string()
+    # Process the CSV file
+    processor = CSVProcessor(config, args)
+    return processor.process()

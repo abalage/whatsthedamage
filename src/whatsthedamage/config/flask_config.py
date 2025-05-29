4538d14f419d791052e4130f2547cf0ebd2a3534
Balázs Németh <6349914+abalage@users.noreply.github.com>
2025-05-29 21:54:11 +0200

    feat: add frontend option to switch languages

--- src/whatsthedamage/config/flask_config.py
+++ src/whatsthedamage/config/flask_config.py
@@ -6,3 +6,5 @@ class FlaskAppConfig:
     MAX_CONTENT_LENGTH: int = 16 * 1024 * 1024  # 16 MB
     SECRET_KEY: bytes = os.urandom(24)
     DEFAULT_WHATSTHEDAMAGE_CONFIG: str = 'config.yml.default'
+    LANGUAGES: list[str] = ['en', 'hu']
+    DEFAULT_LANGUAGE: str = 'en'
